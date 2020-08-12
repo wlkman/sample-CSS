714391b5f5da1e77c6de30c8b00c98325c733dd1
wlkman <cuixinbin@yeah.net>
2020-08-12 08:25:46 +0800

    autoHideNavigator

---
+++ autoHideNavigator/autoHideNavigator/googlegg.js
@@ -0,0 +1,12 @@
+﻿document.writeln("<style type=\"text/css\">");
+document.writeln(".schmenu{height:33px; line-height:33px; background:#636871; width:100%; margin:0 auto; overflow:hidden;}");
+document.writeln(".schmenu ul{width:1180px; margin:0 auto; overflow:hidden;}");
+document.writeln(".schmenu ul li{float:left;list-style:none;}");
+document.writeln(".schmenu ul li a{height:33px; line-height:33px; display:block;color:#fff; font-size: 13px; padding: 0 10px;text-decoration: none;}");
+document.writeln(".schmenu ul li a:hover{color:#fff; background:#000; text-decoration:none;-moz-transition:all 0.2s ease;-webkit-transition:all 0.2s ease;-o-transition:all 0.2s ease;-moz-transition:all 0.2s ease;transition:all 0.2s ease;}");
+document.writeln("<\/style>");
+document.writeln("<div class=\"schmenu\">");
+document.writeln("<ul>");
+document.writeln("<li><a href=\"http://www.lanrenzhijia.com\">返回首页<\/a><\/li>");
+document.writeln("<\/ul>");
+document.writeln("<\/div>");
